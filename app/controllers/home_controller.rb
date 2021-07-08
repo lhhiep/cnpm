83512c4d8d0b9897cc7e4a49dfeed60b418a1e78
lhhiep <hiep.le13499@hcmut.edu.vn>
2021-07-08 22:30:05 +0700

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,18 +6,6 @@ class HomeController < ApplicationController
 		else
 			@food_items = FoodItem.all
 		end
-
-		if params[:section] && params[:section] != ""
-			@food_items = @food_items.where section: params[:section]
-		end
-
-		if params[:food_type] && params[:food_type] != ""
-			@food_items = @food_items.where food_type: params[:food_type]
-		end
-
-		if params[:sort_column]
-			@food_items = @food_items.order("#{params[:sort_column]} #{params[:sort_direction]}")
-		end 
 	end
 	
 end
